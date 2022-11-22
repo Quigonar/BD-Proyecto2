de0dd612c99ccfe24437c13ffd324708c698fea2
Quigonar <67380073+Quigonar@users.noreply.github.com>
2022-11-21 21:38:05 -0600

    api implementation missing

--- src/app/ClientView/feedback/feedback.component.ts
+++ src/app/ClientView/feedback/feedback.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'app/services/api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-feedback',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.component.scss']
 })
 export class FeedbackComponent implements OnInit {
+  private clientID
+  private routeSub: Subscription
 
-  constructor() { }
+  public feedback = new FormGroup({
+    Feed : new FormControl(),
+  })
 
-  ngOnInit(): void {
+  constructor(private api:ApiService, public router:Router, private route:ActivatedRoute) { }
+
+  onSend(form) {
+    console.log(form.Feed)
+    //SEND TO API form.Feed BY CLIENT ID PARAMETER
+    this.router.navigate(['/shopping-cart/', this.clientID])
   }
 
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(params => {
+      this.clientID = params['id']
+      console.log(this.clientID)
+  })
+  }
 }
